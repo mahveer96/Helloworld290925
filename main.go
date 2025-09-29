7fcccb9ece32a68188be7409086c0bc4f506d9aa
Mahveer96 <mahesh.tella.com>
2025-09-29 15:39:22 +0530

    Rest API is added

--- main.go
+++ main.go
@@ -1,8 +1,38 @@
 package main
 import (
+	"encoding/json"
 	"fmt"
+	"log"
+	"net/http"
 )
+
+
+// ✅ Define the Response struct for JSON output
+type Response struct {
+	Message string `json:"message"`
+}
+
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+
+
+	// Dummy hardcoded credentials
+	
+		w.Header().Set("Content-Type", "application/json")
+		json.NewEncoder(w).Encode(Response{Message: "Login successful"})
+
+}
+
+
+func homeHandler(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprintln(w, "Hello world welcome to Golang")
+}
 
 func main(){
 	fmt.Println("Hello world welcome to Golang ")
+    http.HandleFunc("/", homeHandler)
+	http.HandleFunc("/login", loginHandler)
+	fmt.Println("Starting server on :8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
+
+
 }
